64fb090879b46f6202b926d5304e44f2b14ea426
gustavosb64 <gustavosb64@gmail.com>
2021-05-28 10:23:13 -0300

    Modificando um_pinguim_no_gelo.c. Ainda não totalmente funcional: sinais de parada apresentando erros em alguns casos

--- Lista02/um_pinguim_no_gelo/um_pinguim_no_gelo.c
+++ Lista02/um_pinguim_no_gelo/um_pinguim_no_gelo.c
@@ -1,30 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprimirPinguim(int i){
-
-    return;
-}
-
-void imprimirNoGelo(int i){
-
-    return;
-}
-
-void imprimirUhuu(int i){
-
-    return;
-}
+void imprimirPinguim(int k);
+void imprimirNoGelo();
+void imprimirUhuu();
 
 int main(int argc, char *argv[]){
     int n, m, p;
     scanf(" %d %d %d",&n,&m,&p);
 
-    for (int i = 1; i <= n; i++){
-        if (!(i%p)){
-            switch
-        }
+    int i = 0;
+    int k = 1;
+    int cont = 1;
+    int vez = p;
 
+    while (cont <= n){
+        for (i=0; i<k; i++){
+            if (cont == vez){
+                imprimirPinguim(k);
+                vez += m;
+            }
+            cont++;
+        }
+        if (cont > n) break;
+        for (i=0; i<k; i++){
+            if (cont == vez){
+                imprimirNoGelo();
+                vez += m;
+            }
+            cont++;
+        }
+        if (cont > n) break;
+        for (i=0; i<k; i++){
+            if (cont == vez){
+                imprimirUhuu();
+                vez += m;
+            }
+            cont++;
+        }
+        if (cont > n) break;
+        k++;
     }
+
     return 0;
+}
+
+void imprimirPinguim(int k){
+    (k > 1) ? printf("%d pinguins\n", k) : printf("%d pinguim\n", k);
+    return;
+}
+
+void imprimirNoGelo(){
+    printf("no gelo\n");
+    return;
+}
+
+void imprimirUhuu(){
+    printf("uhuu!\n");
+    return;
 }
