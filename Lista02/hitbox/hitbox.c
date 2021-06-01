0aa29605b4ddefa470ce283a360e0fc3459d21b1
gustavosb64 <gustavosb64@gmail.com>
2021-06-01 14:32:56 -0300

    Lista02: hitbox: identificando HIT or MISS

--- Lista02/hitbox/hitbox.c
+++ Lista02/hitbox/hitbox.c
@@ -8,9 +8,14 @@ void swap(int *a,int *b){
     return;
 }
 
-//double Interseccao(p1,l1,p2,l2){
-    
-//}
+int compare_edge(int smaller_e, int length, int bigger_e){
+
+    if (smaller_e + length >= bigger_e){
+        return (smaller_e+length - bigger_e);
+    }
+
+    return -1;
+}
 
 int main(int argc, char *argv[]){
     int x1, y1, l1, h1;
@@ -19,47 +24,55 @@ int main(int argc, char *argv[]){
     scanf(" %d %d %d %d",&x1,&y1,&l1,&h1);
     scanf(" %d %d %d %d",&x2,&y2,&l2,&h2);
 
-    /*intersectam se
-     * x1+l1 >= x2 e y1+a1 >= y2
-     * ou
-     * x2+l2 >= x1 e y2+a2 >= y1
+    /*Comparing x1 to x2
     */
+    int smaller_x, smaller_l;
+    int bigger_x, bigger_l;
+    if (x1 <= x2){
+        smaller_x = x1;
+        smaller_l = l1;
 
-    int menor_x, menor_l, menor_y, menor_h;
-    int maior_x, maior_l, maior_y, maior_h;
-
-    if (x1 < x2){
-        menor_x = x1;
-        menor_l = l1;
-        maior_x = x2;
-        maior_l = l2;
+        bigger_x = x2;
+        bigger_l = l2;
     }
     else{
-        menor_x = x2;
-        menor_l = l2;
-        maior_x = x1;
-        maior_l = l1;
-    }
-    if (y1 < y2){
-        menor_y = y1;
-        menor_h = h1;
-        maior_y = y2;
-        maior_h = h2;
-    }
-    else{
-        menor_y = y2;
-        menor_h = h2;
-        maior_y = y1;
-        maior_h = h1;
+        smaller_x = x2;
+        smaller_l = l2;
+
+        bigger_x = x1;
+        bigger_l = l1;
     }
 
-    if ((menor_x+menor_l < maior_x) || (menor_y+menor_h) < maior_y){
+    /*Comparing y1 to y2
+    */
+    int smaller_y, smaller_h;
+    int bigger_y, bigger_h;
+    if (y1 <= y2){
+        smaller_y = y1;
+        smaller_h = h1;
+
+        bigger_y = y2;
+        bigger_h = h2;
+    }
+    else{
+        smaller_y = y2;
+        smaller_h = h2;
+
+        bigger_y = y1;
+        bigger_h = h1;
+    }
+
+    int intersec_x = compare_edge(smaller_x, smaller_l, bigger_x);
+    int intersec_y = compare_edge(smaller_y, smaller_h, bigger_y);
+    if (intersec_x == -1){
         printf("MISS\n");
         return 0;
     }
-/*
-    int ix = 0, iy = 0, il = 0, ih = 0;
-*/
+    else if (intersec_y == -1){
+        printf("MISS\n");
+        return 0;
+    }
+
     printf("HIT\n");
     return 0;
 }
---
+++ Lista03/contando_palavras/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 31/05/2021 23:28:25 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [3 - Strings] Contando Palavras
