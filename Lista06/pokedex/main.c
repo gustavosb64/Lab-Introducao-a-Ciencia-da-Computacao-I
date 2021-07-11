4c6ba6fc6bde759ecd9de4b8d647797bc8f62a8e
Gustavo Barbosa <gustavosb64@gmail.com>
2021-07-11 00:06:01 -0300

    Lista06: pokedex: COMPLETO

--- Lista06/pokedex/main.c
+++ Lista06/pokedex/main.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//enum para a categoria
-enum Category{Physical = 'p', Special = 's'};
-
 //definindo estruturas
 typedef struct move_{
     char *name;
@@ -57,24 +54,12 @@ void command1(Pokemon *pokemon){
     pokemon->name = readline(stdin);
     pokemon->type1 = readline(stdin);
     pokemon->type2 = readline(stdin);
-    scanf(" %d ", &pokemon->stats.HP);
-    scanf(" %d ", &pokemon->stats.atk);
-    scanf(" %d ", &pokemon->stats.def);
-    scanf(" %d ", &pokemon->stats.spAtk);
-    scanf(" %d ", &pokemon->stats.spDef);
+    scanf(" %d", &pokemon->stats.HP);
+    scanf(" %d", &pokemon->stats.atk);
+    scanf(" %d", &pokemon->stats.def);
+    scanf(" %d", &pokemon->stats.spAtk);
+    scanf(" %d", &pokemon->stats.spDef);
     scanf(" %d", &pokemon->stats.speed);
-
-/*
-    printf("name: %s\n", pokemon.name);
-    printf("type1: %s\n", pokemon.type1);
-    printf("type2: %s\n", pokemon.type2);
-    printf("HP: %d\n", pokemon.stats.HP);
-    printf("atk: %d\n", pokemon.stats.atk);
-    printf("def: %d\n", pokemon.stats.def);
-    printf("spAtk: %d\n", pokemon.stats.spAtk);
-    printf("spDef: %d\n", pokemon.stats.spDef);
-    printf("speed: %d\n", pokemon.stats.speed);
-*/
 
     return;
 }
@@ -83,36 +68,62 @@ void command1(Pokemon *pokemon){
 void command2(Pokemon *pokedex){
 
     int idx_pok, idx_move;
-    scanf (" %d",&idx_pok);
-    scanf (" %d",&idx_move);
+    scanf(" %d",&idx_pok);
+    scanf(" %d",&idx_move);
     getc(stdin);
 
     pokedex[idx_pok].moves[idx_move].name = readline(stdin);
-    scanf(" %d ",&pokedex[idx_pok].moves[idx_move].power);
-    scanf(" %f ",&pokedex[idx_pok].moves[idx_move].accuracy);
+    scanf(" %d",&pokedex[idx_pok].moves[idx_move].power);
+    scanf(" %f",&pokedex[idx_pok].moves[idx_move].accuracy);
     scanf(" %c",&pokedex[idx_pok].moves[idx_move].category);
-
-/*
-    printf("move name: %s\n", pokedex[idx_pok].moves[idx_move].name);
-    printf("move power: %d\n", pokedex[idx_pok].moves[idx_move].power);
-    printf("move accuracy: %.6f\n", pokedex[idx_pok].moves[idx_move].accuracy);
-    printf("move category: %c\n", pokedex[idx_pok].moves[idx_move].category);
-*/
 
     return;
 }
 
+void command3(Pokemon *pokedex){
+
+    int idx_pok;
+    scanf(" %d",&idx_pok);
+
+    printf("Nome do Pokemon: %s\n", pokedex[idx_pok].name);
+    printf("Tipo primario: %s\n", pokedex[idx_pok].type1);
+    printf("Tipo secundario: %s\n", pokedex[idx_pok].type2);
+    printf("Status:\n");
+    printf("\tHP: %d\n", pokedex[idx_pok].stats.HP);
+    printf("\tAtaque: %d\n", pokedex[idx_pok].stats.atk);
+    printf("\tDefesa: %d\n", pokedex[idx_pok].stats.def);
+    printf("\tAtaque Especial: %d\n", pokedex[idx_pok].stats.spAtk);
+    printf("\tDefesa Especial: %d\n", pokedex[idx_pok].stats.spDef);
+    printf("\tVelocidade: %d\n\n", pokedex[idx_pok].stats.speed);
+
+    return;
+}
+
+void command4(Pokemon *pokedex){
+
+    int idx_pok, idx_move;
+    scanf(" %d",&idx_pok);
+    scanf(" %d",&idx_move);
+    getc(stdin);
+
+    printf("Nome do Ataque: %s\n", pokedex[idx_pok].moves[idx_move].name);
+    printf("Poder base: %d\n", pokedex[idx_pok].moves[idx_move].power);
+    printf("Acuracia: %f\n", pokedex[idx_pok].moves[idx_move].accuracy);
+    printf("Classe: %c\n\n", pokedex[idx_pok].moves[idx_move].category);
+
+    return;
+}
 int main(int argc, char *argv[]){
 
     Pokemon *pokedex = (Pokemon *) malloc(1 * sizeof(Pokemon));
     int n_pokemon = 0;
 
-    int command;
-    scanf(" %d ",&command);
+    int option;
+    scanf(" %d ",&option);
 
-    while (command != 0){
+    while (option != 0){
 
-        switch(command){
+        switch(option){
             case 1:
                 command1(&pokedex[n_pokemon]);
                 n_pokemon++;
@@ -121,17 +132,22 @@ int main(int argc, char *argv[]){
 
             case 2:
                 command2(pokedex);
-                command = 0;
                 break;
 
             case 3:
+                command3(pokedex);
                 break;
 
+            case 4:
+                command4(pokedex);
+                break;
         }
 
-        scanf(" %d",&command);
+        scanf(" %d",&option);
+        getc(stdin);
     }
 
+    //liberando memória
     for(int i=0; i<n_pokemon; i++){
         free(pokedex[i].name);
         free(pokedex[i].type1);
